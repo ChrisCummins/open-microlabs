d1033fe73e0590cdbad61df5cf6bc0e6ca330dea
Chris Cummins <chrisc.101@gmail.com>
2012-04-23 15:35:54 +0100

    0.8.0 Major AVR code revision. Program behaviour is performed by status flag checks in the main function, rather than length ISRs.
    Additionally, slave instructions are only transmitted once all local
    instructios have been processed.

--- src-microcontroller/main.c
+++ src-microcontroller/main.c
@@ -1,5 +1,5 @@
 /* main.c - Main program file for AVR microcontrollers.
- * Version 0.6.5 - 19th April 2012.
+ * Version 0.8.0 - 23rd April 2012.
  * Target: ATmega164PA.
  *
  *
@@ -20,34 +20,68 @@
  * along with Open MicroLabs.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// INCLUDES ====================================================================
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <string.h>
 
-// Baud values for desired baud rates at different clock speeds.
-#define UBRR16_9600  0x67               // 9600b/s @ 16MHZ (0.160% error).
-#define UBBR16_19200 0x33               // 19200b/s @ 16MHZ (0.160% error).
-#define UBRR16_38400 0x19               // 38400b/s @ 16MHZ (0.160% error).
-#define AREF_AVCC (1<<REFS0)            // Vcc (5V).
-#define ADC_PRESCALE_127 ((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0))
-#define DESIRED_UBRR0 UBRR16_38400
-#define DESIRED_UBRR1 UBRR16_38400
-#define DESIRED_AREF AREF_AVCC
-#define DESIRED_ADC_PRESCALE ADC_PRESCALE_127
+// MACROS ======================================================================
 
-// Char buffer used for storing ADC values for transmission.
+/* USER CONFIG
+ * BAUD_VALUE           Baud value for the desired baud rate.
+ * AREF_VALUE           ADC reference voltage to use.
+ * ADC_PRESCALE         ADC prescale to use.
+ */
+#define BAUD_VALUE 0x19                 // 38400b/s @ 16MHZ (0.160% error).
+#define AREF_VALUE (1<<REFS0)           // Vcc (5V).
+#define ADC_PRESCALE ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)) // PRESCALE 127.
+/* INSTRUCTION BITS
+ * SIB                  Slave instruction bit.
+ * SIB_OFF_MASK         Value to set SIB low when char &= SIB_OFF_MASK.
+ */
+#define SIB 0
+#define SIB_OFF_MASK 0xFE
+
+/* INTERRUPT FLAGS
+ * UDR0_FLAG            Set by USART0_RX_vect to show that udr0 has been updated.
+ * UDR1_FLAG            Set by USART1_RX_vect to show that udr1 has been updated.
+ */
+#define UDR0_FLAG 0
+#define UDR1_FLAG 1
+
+// GLOBAL VARIABLES ============================================================
+
+/* USART BUFFERS
+ * usart0_tx_buffer     Char buffer used for storing formatted ADC values.
+ * usart0_rx_buffer     Char buffer used for received data request slave
+ *                      instructions.
+ * usart1_rx_buffer     Char buffer used for storing received data request
+ *                      responses from slaves.
+ */
 static char usart0_tx_buffer[5];
+static char usart0_rx_buffer[14];
+static char usart1_rx_buffer[490];
 
-// Char buffer used for received data request slave instructions.
-static char usart0_rx_buffer[28];
+/* STATUS INTEGER
+ * The 8-bit unsigned integer acts as a status register for interrupts. See
+ * interrupt flags.
+ */
+volatile static uint8_t sint;
 
-// Char buffer used for storing received data request responses from slaves.
-static char usart1_rx_buffer[980];
+/* UDR CHARACTERS
+ * Characters used for storing values from UDR0 and UDR1 respectively.
+ */
+static char udr0;
+static char udr1;
+
+// FUNCTIONS ===================================================================
 
 /**
  * Start an ADC conversion and wait until finished.
  */
+static inline __attribute__((always_inline))
 void
 performADC()
 {
@@ -61,25 +95,15 @@ performADC()
     }
 }
 
-/*
- * Add a character to the end of a string.
- */
-void
-appendToString(char *string, char c)
-{
-  // Get string last char index.
-  uint16_t index = strlen(string);
-
-  // Add char to string at index.
-  string[index] = c;
-
-  // Add null char to new end of string.
-  string[index + 1] = '\0';
-}
-
-/*
+/**
  * Transmit a character through USART port.
+ *
+ * @param c             Character to transmit.
+ * @param *udr          UDR port address.
+ * @param *ucsra        UCSRA register address.
+ * @param udre_bit      UDRE bit index within UCSRA.
  */
+static inline __attribute__((always_inline))
 void
 txCharUSART(char c, volatile uint8_t *udr_address,
     volatile uint8_t *ucsra_address, uint8_t udre_bit)
@@ -91,12 +115,19 @@ txCharUSART(char c, volatile uint8_t *udr_address,
   *udr_address = c;
 }
 
-/*
+/**
  * Transmit a string through USART port.
+ *
+ * @param *string       Character array to transmit.
+ * @param index         Integer used to traverse the string.
+ * @param *udr          UDR port address.
+ * @param *ucsra        UCSRA register address.
+ * @param udre_bit      UDRE bit index within UCSRA.
  */
+static inline __attribute__((always_inline))
 void
 txStringUSART(char *string, int index, volatile uint8_t *udr,
-    volatile uint8_t *ucsra_address, uint8_t udre_bit)
+    volatile uint8_t *ucsra, uint8_t udre_bit)
 {
   index = 0;
   while (string[index] != '\0')
@@ -105,11 +136,14 @@ txStringUSART(char *string, int index, volatile uint8_t *udr,
     }
 }
 
-/*
+/**
  * Iterate over each bit of a char and perform ADCs and transmit as required.
+ *
+ * @param c Instruction char to process.
  */
+static inline __attribute__((always_inline))
 void
-processInstruction(char c)
+localInstruction(char c)
 {
   // Iterate over ADC channels [1, 7].
   uint8_t index;
@@ -134,34 +168,25 @@ processInstruction(char c)
 }
 
 /**
- * USART0 Receive character interrupt.
+ * Process USART0_FLAG. If udr0 SIB is set, the char is added to
+ * usart0_rx_buffer. Else, it processes the instruction, transmits an EOL if
+ * necessary, else transmits slave instructions.
+ *
+ * @param c Instruction char to process.
  */
+static inline __attribute__((always_inline))
 void
-USART0_RX_vect(void) __attribute__ ((signal,used, externally_visible));
-void
-USART0_RX_vect(void)
+usart0Flag(char c)
 {
-  // Read char from UDR0.
-  unsigned char c = UDR0;
-
-  if (c & (1 << 0))
+  // If SIB.
+  if (c & (1 << SIB))
     {
       // Add to USART0 RX bufer:
-      appendToString(usart0_rx_buffer, c);
+      usart0_rx_buffer[strlen(usart0_rx_buffer)] = c;
     }
   else
     {
-      if (strlen(usart0_rx_buffer) > 0)
-        {
-          // Disable SIB for last char.
-          uint8_t index = strlen(usart0_rx_buffer) - 1;
-          usart0_rx_buffer[index] &= 0xFE;
-
-          // Transmit slave instructions.
-          txStringUSART(usart0_rx_buffer, index, &UDR1, &UCSR1A, UDRE1);
-        }
-
-      processInstruction(c);
+      localInstruction(c);
 
       // If USART0 RX buffer is empty (no slave instructions).
       if (strlen(usart0_rx_buffer) < 1)
@@ -171,6 +196,13 @@ USART0_RX_vect(void)
         }
       else
         {
+          // Disable SIB for last char.
+          uint8_t index = strlen(usart0_rx_buffer) - 1;
+          usart0_rx_buffer[index] &= SIB_OFF_MASK;
+
+          // Transmit slave instructions.
+          txStringUSART(usart0_rx_buffer, index, &UDR1, &UCSR1A, UDRE1);
+
           // Clear USART0 RX buffer.
           memset(usart0_rx_buffer, 0x0, sizeof(usart0_rx_buffer));
         }
@@ -178,19 +210,18 @@ USART0_RX_vect(void)
 }
 
 /**
- * USART1 Receive character interrupt: add to buffer and transmit if EOL.
+ * Process USART1_FLAG. Adds char to usart1_rx_buffer, then transmits buffer
+ * contents if char is EOL.
+ *
+ * @param c Slave response character to process.
  */
+static inline __attribute__((always_inline))
 void
-USART1_RX_vect(void) __attribute__ ((signal,used, externally_visible));
-void
-USART1_RX_vect(void)
+usart1Flag(char c)
 {
-  // Read char from UDR1.
-  unsigned char c = UDR1;
+  usart1_rx_buffer[strlen(usart1_rx_buffer)] = c;
 
-  appendToString(usart1_rx_buffer, c);
-
-  // If char is EOL,
+  // If character is EOL.
   if (c == '\r')
     {
       // Transmit USART1 RX buffer:
@@ -202,22 +233,18 @@ USART1_RX_vect(void)
     }
 }
 
+/**
+ * Main method. Sets up ADC, USART1, USART0, global interrupts and then performs
+ * flag checks indefinitely.
+ */
 int
 main(void)
 {
-  // Set USART bauds.
-  UBRR0 = DESIRED_UBRR0;
-  UBRR1 = DESIRED_UBRR1;
-
-  // Enable rx/tx/rxi for USART0 and USART1.
-  UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
-  UCSR1B |= (1 << RXEN1) | (1 << TXEN1) | (1 << RXCIE1);
-
   // Setup ADC AREF.
-  ADMUX = DESIRED_AREF;
+  ADMUX = AREF_VALUE;
 
   // Set ADC Prescale.
-  ADCSRA |= (DESIRED_ADC_PRESCALE | (1 << ADEN) | (1 << ADSC));
+  ADCSRA |= ((ADC_PRESCALE) | (1 << ADEN) | (1 << ADSC));
 
   // Set free running mode.
   ADCSRB = 0x00;
@@ -225,12 +252,57 @@ main(void)
   // Perform Dummy read of ADC.
   performADC();
 
+  // Set USART bauds.
+  UBRR1 = BAUD_VALUE;
+  UBRR0 = BAUD_VALUE;
+
+  // Enable rx/tx/rxi for USART0 and USART1.
+  UCSR1B |= (1 << RXEN1) | (1 << TXEN1) | (1 << RXCIE1);
+  UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+
   // Enable interrupts.
   sei();
 
   // Do nothing.
   while (1)
     {
-      asm("NOP");
+      // Test UDR0_FLAG.
+      if (sint & (1 << UDR0_FLAG))
+        {
+          usart0Flag(udr0);
+          sint &= (0 << UDR0_FLAG);
+        }
+      // TEST UDR1_FLAG.
+      if (sint & (1 << UDR1_FLAG))
+        {
+          usart1Flag(udr1);
+          sint &= (0 << UDR1_FLAG);
+        }
     }
+}
+
+// INTERRUPT VECTORS ===========================================================
+
+/**
+ * USART0 Receive character interrupt: store char and set UDR0_flag.
+ */
+void
+USART0_RX_vect(void) __attribute__ ((signal,used, externally_visible));
+void
+USART0_RX_vect(void)
+{
+  udr0 = UDR0;
+  sint |= (1 << UDR0_FLAG);
+}
+
+/**
+ * USART1 Receive character interrupt: store char and set UDR1_FLAG.
+ */
+void
+USART1_RX_vect(void) __attribute__ ((signal,used, externally_visible));
+void
+USART1_RX_vect(void)
+{
+  udr1 = UDR1;
+  sint |= (1 << UDR1_FLAG);
 }
